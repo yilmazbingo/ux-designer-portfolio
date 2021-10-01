ae444ef49c702ee7a420968272c992033577c135
yilmaz <yilmazapis@gmail.com>
2021-10-01 04:42:47 -0400

    initail commit

--- src/app.js
+++ src/app.js
@@ -7,8 +7,12 @@ import Brand from "./assets/img/portfolio-branding-02-1.jpg";
 import Brand1 from "./assets/img/portfolio-branding-01-1.jpg";
 import Print from "./assets/img/portfolio-print-02-1.jpg";
 import Content from "./assets/img/full-bg-03.png";
-import Testimonial from "./assets/img/testimonials-1.jpg";
-import Testimonial2 from "./assets/img/testimonials-2.jpg";
+import Testimonial from "./assets/img/testimonials-1.png";
+import Testimonial3 from "./assets/img/testimaniols-3.png";
+import PortfolioDesign from "./assets/img/portfolio-design.png";
+import Developer from "./assets/img/developer.png";
+
+import Testimonial2 from "./assets/img/testimonials-2.png";
 import Testimonial4 from "./assets/img/testimonials-4.jpg";
 const App = () => (
   <body>
@@ -18,23 +22,20 @@ const App = () => (
           <img src={logo} alt="logo" />
         </div>
         <ul class="side-nav__content--social">
-          <a href="#">
-            <i class="fab fa-facebook-f"></i>
+          <a href="https://github.com/yilmazbingo">
+            <i class="fab fa-github"></i>
           </a>
-          <a href="#">
+          <a href="https://twitter.com/bingolyilmazny">
             <i class="fab fa-twitter"></i>
           </a>
-          <a href="#">
+          <a href="https://www.linkedin.com/feed/">
             <i class="fab fa-linkedin-in"></i>
           </a>
-          <a href="#">
+          <a href="https://www.instagram.com/yilmazcik/">
             <i class="fab fa-instagram"></i>
           </a>
-          <a href="#">
-            <i class="fab fa-dribbble"></i>
-          </a>
-          <a href="#">
-            <i class="fab fa-pinterest-p"></i>
+          <a href="https://stackoverflow.com/users/10262805/yilmaz">
+            <i class="fab fa-stack-overflow"></i>
           </a>
         </ul>
       </div>
@@ -53,19 +54,19 @@ const App = () => (
               <a href="#">Home</a>
             </li>
             <li>
-              <a href="#">About</a>
+              <a href="#about-me">About</a>
             </li>
             <li>
-              <a href="#">Projects</a>
+              <a href="#testimonials">Projects</a>
             </li>
             <li>
-              <a href="#">Services</a>
+              <a href="#services">Services</a>
             </li>
             <li>
-              <a href="#">Hire Me</a>
+              <a href="#skills">Hire Me</a>
             </li>
             <li>
-              <a href="#">Contact</a>
+              <a href="#contact">Contact</a>
             </li>
           </ul>
         </nav>
@@ -77,6 +78,10 @@ const App = () => (
           <p class="showcase__content--para">
             UX Designer / Frontend Developer
           </p>
+          <p class="showcase__content--para">
+            The goal of this project to get hired
+          </p>
+
           <a href="#" class="showcase__content--link">
             My Work
           </a>
@@ -193,6 +198,88 @@ const App = () => (
         </div>
       </section>
 
+      <section id="testimonials">
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={Testimonial3} alt="tony" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">Mobile Apps</h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={Testimonial} alt="natasha" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">Catchy Designs</h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+      </section>
+      <section id="testimonials">
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={PortfolioDesign} alt="tony" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">Remarkable Eye</h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={Testimonial2} alt="natasha" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">Ecommerce</h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={Developer} alt="veers" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">Engineering</h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+
+        <div class="testimonials__card">
+          <div class="testimonials__card--img">
+            <img src={Testimonial4} alt="odinson" />
+          </div>
+          <div class="testimonials__card--content">
+            <h3 class="testimonials__card--content__title">
+              Easy to work with
+            </h3>
+            <p class="testimonials__card--content__para">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              sunt quis cupiditate ducimus non esse?
+            </p>
+          </div>
+        </div>
+      </section>
       <section id="skills">
         <div class="skills__design">
           <h3 class="skills__design--title">Design Skills</h3>
@@ -239,62 +326,6 @@ const App = () => (
           <h4>SQL</h4>
           <div class="skills__coding--progress">
             <span class="skills__coding--progress__4"></span>
-          </div>
-        </div>
-      </section>
-
-      <section id="testimonials">
-        <div class="testimonials__card">
-          <div class="testimonials__card--img">
-            <img src={Testimonial} alt="tony" />
-          </div>
-          <div class="testimonials__card--content">
-            <h3 class="testimonials__card--content__title">Remarkable Eye</h3>
-            <p class="testimonials__card--content__para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              sunt quis cupiditate ducimus non esse?
-            </p>
-          </div>
-        </div>
-
-        <div class="testimonials__card">
-          <div class="testimonials__card--img">
-            <img src={Testimonial2} alt="natasha" />
-          </div>
-          <div class="testimonials__card--content">
-            <h3 class="testimonials__card--content__title">Catchy Designs</h3>
-            <p class="testimonials__card--content__para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              sunt quis cupiditate ducimus non esse?
-            </p>
-          </div>
-        </div>
-
-        <div class="testimonials__card">
-          <div class="testimonials__card--img">
-            <img src={Testimonial} alt="veers" />
-          </div>
-          <div class="testimonials__card--content">
-            <h3 class="testimonials__card--content__title">Smoking Websites</h3>
-            <p class="testimonials__card--content__para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              sunt quis cupiditate ducimus non esse?
-            </p>
-          </div>
-        </div>
-
-        <div class="testimonials__card">
-          <div class="testimonials__card--img">
-            <img src={Testimonial4} alt="odinson" />
-          </div>
-          <div class="testimonials__card--content">
-            <h3 class="testimonials__card--content__title">
-              Easy to work with
-            </h3>
-            <p class="testimonials__card--content__para">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              sunt quis cupiditate ducimus non esse?
-            </p>
           </div>
         </div>
       </section>
